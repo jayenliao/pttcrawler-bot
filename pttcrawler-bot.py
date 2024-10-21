1acdf0f1667217133aa45843a417a0be4e214031
jayenliao <jay.liao@fazzfinancial.com>
2024-10-21 10:30:04 +0800

    feat(JAY-784): allow '爬蟲' to trigger crawl_ptt() function

--- pttcrawler-bot.py
+++ pttcrawler-bot.py
@@ -3,6 +3,8 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from bs4 import BeautifulSoup
+import requests
 
 #line token
 channel_access_token = 'i14cG7TnOMjMK1+8AZwUx0DM2LuqEjMvxVXg3kD7dfQfZyjXJrBNIp8snvqeo7prYOrOSPgX/QwGuRJ88Vuc8D/lsgC1P7KgBUl6q6AgHeuSxIvF3PiOy3m+RHQBq72Lnfx/tFohAjk9m9pvZsAVOwdB04t89/1O/w1cDnyilFU='
@@ -31,12 +33,41 @@ def callback():
         abort(400)
     return 'OK'
 
+# 爬蟲函數: 爬取 PTT 的最新文章標題
+def crawl_ptt():
+    url = 'https://www.ptt.cc/bbs/Gossiping/index.html'
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    cookies = {'over18': '1'}  # 設定cookie繞過18歲驗證
+    response = requests.get(url, headers=headers, cookies=cookies)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    titles = []
+    for title in soup.select('.title a'):
+        titles.append(title.text)
+
+    # 回傳前五個標題作為範例
+    return titles[:5]
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    #echo
-    msg= event.message.text
-    message = TextSendMessage(text=msg+"yoyoyoyo")
-    line_bot_api.reply_message(event.reply_token,message)
+    user_message = event.message.text
+
+    # 如果使用者輸入 "爬蟲"，觸發爬取 PTT 的文章標題
+    if user_message == "爬蟲":
+        ptt_titles = crawl_ptt()  # 執行爬蟲函數
+        reply = "\n".join(ptt_titles)  # 格式化回應內容
+
+        # 回傳爬取結果給使用者
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=f"PTT 最新文章標題:\n{reply}")
+        )
+    else:
+        # 回傳原始訊息
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=user_message)
+        )
 
 import os
 if __name__ == "__main__":
